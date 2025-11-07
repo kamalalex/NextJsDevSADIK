10617b940ecb3107b6aff6e8a347786cd48f0fc6
KamalAlex <rmi.search@gmail.Com>
2025-11-07 23:56:13 +0100

    07/11/2025-1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className=" text-4xl font-semibold text-red-900 flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     Let's Do It !
+     Let's Do It !?
     </div>
   );
 }
